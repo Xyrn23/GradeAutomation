7bc206a989db3986f14d3585b52b6c8dab1e4a5d
Xyrn23 <jerseyaaronpaulreyes@gmqil.com>
2023-08-17 13:04:15 +0800

    Updating Readme

--- README.md
+++ README.md
@@ -22,167 +22,74 @@ Finals(40%) |40
 **FinalGrade** | 100
 
 ```python
-# automated grade computation
-# this program only works on my school grading system
-# if you have the same grading system as mine then you can use this
-# else you can't Xd
-#but feel free to use this for testing
-# I herebly take the owner of this code so if you will use on your project,
-# a little credits will do. ty
-from colorama import Fore, init
-
-init(autoreset=True)
-
+from colorama import Fore
 
 def logo():
-  print(Fore.GREEN + """ 
-
+    print(Fore.GREEN + """ 
  __    ___    __    ___   ____                                
 / /`_ | |_)  / /\  | | \ | |_                                 
 \_\_/ |_| \ /_/--\ |_|_/ |_|__                                
  __     __    _     __    _     _      __   _____  ___   ___  
 / /`   / /\  | |   / /`  | | | | |    / /\   | |  / / \ | |_) 
-\_\_, /_/--\ |_|__ \_\_, \_\_/ |_|__ /_/--\  |_|  \_\_/ |_| \ 
-
-[You just need to enter your grade(0-100) and let the tool to the work]
+\_\_, /_/--\ |_|__ \_\_, \_\_/ |_|__ /_/--\  |_|  \_\_/ |_| \\
+[You just need to enter your grade(0-100) and let the tool do the work]
 """)
-
-
-logo()
-
-
+    
 def get_input(prompt):
-  return input(Fore.GREEN + prompt + Fore.RESET)
-
+    while True:
+        try:
+            value = float(input(f"\033[32m{prompt}\033[0m"))
+            if 0 <= value <= 100:
+                return value
+            else:
+                print("\033[31mInvalid input. Please enter a valid grade between 0 and 100.\033[0m")
+        except ValueError:
+            print("\033[31mInvalid input. Please enter a valid numeric value.\033[0m")
 
 def get_quarter_weight(quarter):
-  if quarter == "prelim":
-    return 0.30
-  elif quarter == "midterm":
-    return 0.30
-  elif quarter == "finals":
-    return 0.40
-  else:
-    print("Invalid quarter choice. Using default weight of 0.30 for Prelim.")
-    return 0.30
+    if quarter == "prelim":
+        return 0.30
+    elif quarter == "midterm":
+        return 0.30
+    elif quarter == "finals":
+        return 0.40
+    else:
+        print("Invalid quarter choice. Using default weight of 0.30 for Prelim.")
+        return 0.30
 
+def calculate_grade(criteria, num_weeks, weight):
+    print(f"\033[33m\033[1m>> {criteria} <<\033[0m")
+    grades = [get_input(f"Enter your grade for {criteria} Week {i + 1}~~>") for i in range(num_weeks)]
+    total_grade = sum(grades)
+    average_grade = (total_grade / num_weeks) * weight
+    print(f"\033[33mYour grade in {criteria} is \033[31m{average_grade:.2f}%\033[33m.\033[0m\n")
+    return average_grade
 
-def attendance():
-  print(Fore.YELLOW + "\033[1m>> ATTENDANCE <<\033[0m")
-  attendanceGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for Attendance Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  attendanceGrade = (sum(attendanceGrades) / 5) * 0.1
-  print(
-      f"{Fore.YELLOW}Your grade in Attendance is {Fore.RED}{attendanceGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return attendanceGrade
+def main():
+    logo()
 
+    attendance_grade = calculate_grade("ATTENDANCE", 5, 0.10)
+    aae_grade = calculate_grade("AAE", 5, 0.35)
+    eval_grade = calculate_grade("EVALUATION", 5, 0.30)
+    project_grade = calculate_grade("PROJECT", 5, 0.25)
 
-def aae():
-  print(Fore.YELLOW + "\033[1m>> AAE <<\033[0m")
-  aaeGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for AAE Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  aaeGrade = (sum(aaeGrades) / 5) * 0.35
-  print(
-      f"{Fore.YELLOW}Your grade in AAE is {Fore.RED}{aaeGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return aaeGrade
+    final_onsite_grade = (attendance_grade + aae_grade + eval_grade + project_grade) * 0.50
+    print(f"\033[32mYour final OnSite grade is: \033[31m{final_onsite_grade:.2f}%\033[0m\n")
 
+    lms_grade = calculate_grade("LMS", 5, 0.30)
+    major_exam_score = get_input("Enter your score for the Major Exam (out of 50)~~>")
+    major_exam_grade = (major_exam_score / 50) * 0.20
+    print(f"\033[33mYour grade in Major Exam is \033[31m{major_exam_grade:.2f}%\033[0m\n")
 
-def evaluation():
-  print(Fore.YELLOW + "\033[1m>> EVALUATION <<\033[0m")
-  evalGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for Evaluation Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  evalGrade = (sum(evalGrades) / 5) * 0.3
-  print(
-      f"{Fore.YELLOW}Your grade in Evaluation is {Fore.RED}{evalGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return evalGrade
+    final_onsite_lms_major_grade = final_onsite_grade + lms_grade + major_exam_grade
+    print(f"\033[32mYour final grade (including LMS and Major Exam) is: \033[31m{final_onsite_lms_major_grade:.2f}%\033[0m")
 
+    quarter = input("\033[32mEnter the quarter (prelim/midterm/finals): \033[0m")
+    quarter_weight = get_quarter_weight(quarter)
+    final_grade = final_onsite_lms_major_grade * quarter_weight
+    print(f"\033[32mYour final grade (including LMS and Major Exam) for {quarter} is: \033[31m{final_grade:.2f}%\033[0m")
 
-def project():
-  print(Fore.YELLOW + "\033[1m>> PROJECT <<\033[0m")
-  projectGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for Project {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  projectGrade = (sum(projectGrades) / 5) * 0.25
-  print(
-      f"{Fore.YELLOW}Your grade in Project is {Fore.RED}{projectGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return projectGrade
+if __name__ == "__main__":
+    main()
 
-
-attendanceGrade = attendance()
-aaeGrade = aae()
-evalGrade = evaluation()
-projectGrade = project()
-
-final_onsite_grade = (attendanceGrade + aaeGrade + evalGrade +
-                      projectGrade) * 0.50
-print(
-    Fore.GREEN +
-    f"Your final OnSite grade is: {Fore.RED}{final_onsite_grade:.2f}%{Fore.RESET}\n"
-)
-
-
-def lms():
-  print(Fore.YELLOW + "\033[1m>> LMS <<\033[0m")
-  lmsGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for LMS Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  lmsGrade = (sum(lmsGrades) / 5) * 0.3
-  print(
-      f"{Fore.YELLOW}Your grade in LMS is {Fore.RED}{lmsGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return lmsGrade
-
-
-def major_exam():
-  print(Fore.YELLOW + "\033[1m>> MAJOR EXAM <<\033[0m")
-  major_exam_score = float(
-      get_input(
-          f"{Fore.GREEN}Enter your score for the Major Exam (out of 50)~~>{Fore.RESET}"
-      ))
-  major_exam_grade = (major_exam_score / 50) * 20
-  print(
-      f"{Fore.YELLOW}Your grade in Major Exam is {Fore.RED}{major_exam_grade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return major_exam_grade
-
-
-lmsGrade = lms()
-major_exam_grade = major_exam()
-
-# Calculate and print final grades
-final_onsite_lms_major_grade = final_onsite_grade + lmsGrade + major_exam_grade
-print(
-    Fore.GREEN +
-    f"Your final grade (including LMS and Major Exam) is: {Fore.RED}{final_onsite_lms_major_grade:.2f}%{Fore.RESET}"
-)
-
-quarter = input(Fore.GREEN + "Enter the quarter (prelim/midterm/finals): ")
-quarter_weight = get_quarter_weight(quarter)
-final_grade = final_onsite_lms_major_grade * quarter_weight
-print(
-    Fore.GREEN +
-    f"Your final grade (including LMS and Major Exam) for {quarter} is: {Fore.RED}{final_grade:.2f}%{Fore.RESET}"
-)
 ```
--- grades.py
+++ grades.py
@@ -1,163 +1,95 @@
-# automated grade computation
-# this program only works on my school grading system
-# if you have the same grading system as mine then you can use this
-# else you can't Xd
-#but feel free to use this for testing
-# I herebly take the owner of this code so if you will use on your project,
-# a little credits will do. ty
-from colorama import Fore, init
+# [this tool is excusively for BCP students who has the same grading as I am on understanding yourself]
+# but feel free to use this for testing
+# I herebly take the owner of this code so if you will use on your project, a little credits will do. ty
+from colorama import init, Fore
 
 init(autoreset=True)
 
-
 def logo():
-  print(Fore.GREEN + """ 
-
+    print(Fore.GREEN + """ 
  __    ___    __    ___   ____                                
 / /`_ | |_)  / /\  | | \ | |_                                 
 \_\_/ |_| \ /_/--\ |_|_/ |_|__                                
  __     __    _     __    _     _      __   _____  ___   ___  
 / /`   / /\  | |   / /`  | | | | |    / /\   | |  / / \ | |_) 
-\_\_, /_/--\ |_|__ \_\_, \_\_/ |_|__ /_/--\  |_|  \_\_/ |_| \ 
-
+\_\_, /_/--\ |_|__ \_\_, \_\_/ |_|__ /_/--\  |_|  \_\_/ |_| \
 [You just need to enter your grade(0-100) and let the tool to the work]
 """)
-
-
 logo()
 
-
 def get_input(prompt):
-  return input(Fore.GREEN + prompt + Fore.RESET)
-
+    return input(Fore.GREEN + prompt + Fore.RESET)
 
 def get_quarter_weight(quarter):
-  if quarter == "prelim":
-    return 0.30
-  elif quarter == "midterm":
-    return 0.30
-  elif quarter == "finals":
-    return 0.40
-  else:
-    print("Invalid quarter choice. Using default weight of 0.30 for Prelim.")
-    return 0.30
-
+    if quarter == "prelim":
+        return 0.30
+    elif quarter == "midterm":
+        return 0.30
+    elif quarter == "finals":
+        return 0.40
+    else:
+        print("Invalid quarter choice. Using default weight of 0.30 for Prelim.")
+        return 0.30
 
 def attendance():
-  print(Fore.YELLOW + "\033[1m>> ATTENDANCE <<\033[0m")
-  attendanceGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for Attendance Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  attendanceGrade = (sum(attendanceGrades) / 5) * 0.1
-  print(
-      f"{Fore.YELLOW}Your grade in Attendance is {Fore.RED}{attendanceGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return attendanceGrade
-
+    print(Fore.YELLOW + "\033[1m>> ATTENDANCE <<\033[0m")
+    attendanceGrades = [float(get_input(f"{Fore.GREEN}Enter your grade for Attendance Week {i + 1}~~>{Fore.RESET}")) for i in range(5)]
+    attendanceGrade = (sum(attendanceGrades) / 5) * 0.1
+    print(f"{Fore.YELLOW}Your grade in Attendance is {Fore.RED}{attendanceGrade:.2f}%{Fore.YELLOW}.\n")
+    return attendanceGrade
 
 def aae():
-  print(Fore.YELLOW + "\033[1m>> AAE <<\033[0m")
-  aaeGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for AAE Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  aaeGrade = (sum(aaeGrades) / 5) * 0.35
-  print(
-      f"{Fore.YELLOW}Your grade in AAE is {Fore.RED}{aaeGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return aaeGrade
-
+    print(Fore.YELLOW + "\033[1m>> AAE <<\033[0m")
+    aaeGrades = [float(get_input(f"{Fore.GREEN}Enter your grade for AAE Week {i + 1}~~>{Fore.RESET}")) for i in range(5)]
+    aaeGrade = (sum(aaeGrades) / 5) * 0.35
+    print(f"{Fore.YELLOW}Your grade in AAE is {Fore.RED}{aaeGrade:.2f}%{Fore.YELLOW}.\n")
+    return aaeGrade
 
 def evaluation():
-  print(Fore.YELLOW + "\033[1m>> EVALUATION <<\033[0m")
-  evalGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for Evaluation Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  evalGrade = (sum(evalGrades) / 5) * 0.3
-  print(
-      f"{Fore.YELLOW}Your grade in Evaluation is {Fore.RED}{evalGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return evalGrade
-
+    print(Fore.YELLOW + "\033[1m>> EVALUATION <<\033[0m")
+    evalGrades = [float(get_input(f"{Fore.GREEN}Enter your grade for Evaluation Week {i + 1}~~>{Fore.RESET}")) for i in range(5)]
+    evalGrade = (sum(evalGrades) / 5) * 0.3
+    print(f"{Fore.YELLOW}Your grade in Evaluation is {Fore.RED}{evalGrade:.2f}%{Fore.YELLOW}.\n")
+    return evalGrade
 
 def project():
-  print(Fore.YELLOW + "\033[1m>> PROJECT <<\033[0m")
-  projectGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for Project {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  projectGrade = (sum(projectGrades) / 5) * 0.25
-  print(
-      f"{Fore.YELLOW}Your grade in Project is {Fore.RED}{projectGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return projectGrade
-
+    print(Fore.YELLOW + "\033[1m>> PROJECT <<\033[0m")
+    projectGrades = [float(get_input(f"{Fore.GREEN}Enter your grade for Project {i + 1}~~>{Fore.RESET}")) for i in range(5)]
+    projectGrade = (sum(projectGrades) / 5) * 0.25
+    print(f"{Fore.YELLOW}Your grade in Project is {Fore.RED}{projectGrade:.2f}%{Fore.YELLOW}.\n")
+    return projectGrade
 
 attendanceGrade = attendance()
 aaeGrade = aae()
 evalGrade = evaluation()
 projectGrade = project()
 
-final_onsite_grade = (attendanceGrade + aaeGrade + evalGrade +
-                      projectGrade) * 0.50
-print(
-    Fore.GREEN +
-    f"Your final OnSite grade is: {Fore.RED}{final_onsite_grade:.2f}%{Fore.RESET}\n"
-)
-
+final_onsite_grade = (attendanceGrade + aaeGrade + evalGrade + projectGrade) * 0.50
+print(Fore.GREEN + f"Your final OnSite grade is: {Fore.RED}{final_onsite_grade:.2f}%{Fore.RESET}\n")
 
 def lms():
-  print(Fore.YELLOW + "\033[1m>> LMS <<\033[0m")
-  lmsGrades = [
-      float(
-          get_input(
-              f"{Fore.GREEN}Enter your grade for LMS Week {i + 1}~~>{Fore.RESET}"
-          )) for i in range(5)
-  ]
-  lmsGrade = (sum(lmsGrades) / 5) * 0.3
-  print(
-      f"{Fore.YELLOW}Your grade in LMS is {Fore.RED}{lmsGrade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return lmsGrade
-
+    print(Fore.YELLOW + "\033[1m>> LMS <<\033[0m")
+    lmsGrades = [float(get_input(f"{Fore.GREEN}Enter your grade for LMS Week {i + 1}~~>{Fore.RESET}")) for i in range(5)]
+    lmsGrade = (sum(lmsGrades) / 5) * 0.3
+    print(f"{Fore.YELLOW}Your grade in LMS is {Fore.RED}{lmsGrade:.2f}%{Fore.YELLOW}.\n")
+    return lmsGrade
 
 def major_exam():
-  print(Fore.YELLOW + "\033[1m>> MAJOR EXAM <<\033[0m")
-  major_exam_score = float(
-      get_input(
-          f"{Fore.GREEN}Enter your score for the Major Exam (out of 50)~~>{Fore.RESET}"
-      ))
-  major_exam_grade = (major_exam_score / 50) * 20
-  print(
-      f"{Fore.YELLOW}Your grade in Major Exam is {Fore.RED}{major_exam_grade:.2f}%{Fore.YELLOW}.\n"
-  )
-  return major_exam_grade
-
+    print(Fore.YELLOW + "\033[1m>> MAJOR EXAM <<\033[0m")
+    major_exam_score = float(get_input(f"{Fore.GREEN}Enter your score for the Major Exam (out of 50)~~>{Fore.RESET}"))
+    major_exam_grade = (major_exam_score / 50) * 20
+    print(f"{Fore.YELLOW}Your grade in Major Exam is {Fore.RED}{major_exam_grade:.2f}%{Fore.YELLOW}.\n")
+    return major_exam_grade
 
 lmsGrade = lms()
 major_exam_grade = major_exam()
 
 # Calculate and print final grades
 final_onsite_lms_major_grade = final_onsite_grade + lmsGrade + major_exam_grade
-print(
-    Fore.GREEN +
-    f"Your final grade (including LMS and Major Exam) is: {Fore.RED}{final_onsite_lms_major_grade:.2f}%{Fore.RESET}"
-)
+print(Fore.GREEN + f"Your final grade (including LMS and Major Exam) is: {Fore.RED}{final_onsite_lms_major_grade:.2f}%{Fore.RESET}")
 
 quarter = input(Fore.GREEN + "Enter the quarter (prelim/midterm/finals): ")
 quarter_weight = get_quarter_weight(quarter)
 final_grade = final_onsite_lms_major_grade * quarter_weight
-print(
-    Fore.GREEN +
-    f"Your final grade (including LMS and Major Exam) for {quarter} is: {Fore.RED}{final_grade:.2f}%{Fore.RESET}"
-)
+print(Fore.GREEN + f"Your final grade (including LMS and Major Exam) for {quarter} is: {Fore.RED}{final_grade:.2f}%{Fore.RESET}")
+
